38da2dfddd6ff747fd6b4e7d75c451041389bd77
Dahveyea <dahveyea.cowan@gmail.com>
2020-07-01 19:50:46 -0700

    'Users can submit and save posts

---
+++ models/post.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const PostSchema = new Schema({
+  title: { type: String, required: true },
+  url: { type: String, required: true },
+  summary: { type: String, required: true }
+});
+
+module.exports = mongoose.model("Post", PostSchema);
+
+// CREATE
+  app.post('/posts/new', (req, res) => {
+    // INSTANTIATE INSTANCE OF POST MODEL
+    const post = new Post(req.body);
+
+    // SAVE INSTANCE OF POST MODEL TO DB
+    post.save((err, post) => {
+      // REDIRECT TO THE ROOT
+      return res.redirect(`/`);
+    })
+  });
+
+};
--- server.js
+++ server.js
@@ -2,6 +2,26 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+// Assign dependencies to variables so that their methods can be accessed anywhere in the project.
+const expressValidator = require('express-validator');
+const bodyParser = require('body-parser');
+
+
+
+// // The following line must appear AFTER const app = express() and before your routes!
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(expressValidator());
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+
+
+// Set db
+require('./data/reddit-db');
+
+// Dotenv library used to encrypt passwords for security -> 'salting' a password
+require('./controllers/posts.js')(app);
