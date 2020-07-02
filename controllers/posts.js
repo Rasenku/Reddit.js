bea6ed26812dec404db80a96792b44c87fc83ab3
Dahveyea <dahveyea.cowan@gmail.com>
2020-07-01 23:12:59 -0700

    Users must be logged in to submit a post

--- controllers/posts.js
+++ controllers/posts.js
@@ -13,17 +13,33 @@ module.exports = app => {
 })
 
 });
-  // CREATE
-  app.post('/posts/new', (req, res) => {
-    // INSTANTIATE INSTANCE OF POST MODEL
-    const post = new Post(req.body);
 
-    // SAVE INSTANCE OF POST MODEL TO DB
-    post.save((err, post) => {
-      // REDIRECT TO THE ROOT
+// CREATE
+app.post("/posts/new", (req, res) => {
+  if (req.user) {
+    var post = new Post(req.body);
+
+    post.save(function(err, post) {
       return res.redirect(`/`);
-    })
-  });
+    });
+  } else {
+    return res.status(401); // UNAUTHORIZED
+  }
+});
+
+  // INDEX
+    app.get('/', (req, res) => {
+        var currentUser = req.user;
+        // res.render('home', {});
+        console.log(req.cookies);
+        Post.find().populate('author').lean()
+        .then(posts => {
+            res.render('posts-index', { posts, currentUser });
+            // res.render('home', {});
+        }).catch(err => {
+            console.log(err.message);
+        })
+	})
 
 
   // SUBREDDIT
--- server.js
+++ server.js
@@ -20,6 +20,25 @@ app.listen(port, () => console.log(`Example app listening at http://localhost:${
 
 
 
+
+
+var checkAuth = (req, res, next) => {
+  console.log("Checking authentication");
+  if (typeof req.cookies.nToken === "undefined" || req.cookies.nToken === null) {
+    req.user = null;
+  } else {
+    var token = req.cookies.nToken;
+    var decodedToken = jwt.decode(token, { complete: true }) || {};
+    req.user = decodedToken.payload;
+  }
+
+  next();
+};
+
+app.use(checkAuth);
+
+
+
 // Set db
 require('./data/reddit-db');
 
